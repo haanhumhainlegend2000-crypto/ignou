f57823c33000c1cabbeb613d1679e850c45fb9b3
haanhumhainlegend2000-crypto <haanhumhainlegend2000@gmail.com>
2026-04-28 22:51:47 +0530

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,27 +1,5 @@
 
 
-// Custom cursor
-const cursor = document.getElementById('cursor');
-const ring = document.getElementById('cursor-ring');
-
-let mx = 0, my = 0, rx = 0, ry = 0;
-
-if (cursor && ring) {
-    document.addEventListener('mousemove', e => {
-        mx = e.clientX;
-        my = e.clientY;
-        cursor.style.left = mx + 'px';
-        cursor.style.top = my + 'px';
-    });
-
-    (function lerp() {
-        rx += (mx - rx) * 0.12;
-        ry += (my - ry) * 0.12;
-        ring.style.left = rx + 'px';
-        ring.style.top = ry + 'px';
-        requestAnimationFrame(lerp);
-    })();
-}
 
 // Nav scroll state
 const nav = document.getElementById('mainNav');
